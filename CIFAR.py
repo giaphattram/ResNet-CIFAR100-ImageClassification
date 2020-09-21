55caab7dda7984147a27dd5ffd4feb95738e1fc7
giaphattram <giaphattram@gmail.com>
2020-09-21 21:16:22 +0200

    Updating CIFAR.py and data_loader.py

--- CIFAR.py
+++ CIFAR.py
@@ -4,36 +4,30 @@ Created on Sun Sep 20 12:53:20 2020
 
 @author: Admin
 """
-parent_dictionary = "C:\MountedDrive\Projects\Resnet CIFAR100"
+# parent_dictionary = "C:\MountedDrive\Projects\Resnet CIFAR100"
 import os
-os.chdir(parent_dictionary)
+# os.chdir(parent_dictionary)
 import pickle
 from PIL import Image
-import numpy as np
 import pandas as pd
-
+from tqdm import tqdm
 class CIFAR(object):
-    def __init__(self, data_folder = './data'):
+    def __init__(self, mode, data_folder = './data'):
         '''
         '''
+        assert mode == 'train' or mode == 'test'
+        self.mode = mode
         self.data_folder = data_folder
         
-        self.train_dict          = self.get_data_dict('train')
-        self.train_file_names    = self.get_file_names(self.train_dict)
-        self.train_fine_labels   = self.get_fine_labels(self.train_dict)
-        self.train_coarse_labels = self.get_coarse_labels(self.train_dict)
-        self.train_images        = self.get_images(self.train_dict)
-        # self.save_images(self.train_images, self.train_file_names, mode = 'train')
-        self.create_annotation_file(mode = 'train')
+        self.data_dict          = self.get_data_dict()
+        self.file_names         = self.get_file_names(self.data_dict)
+        self.fine_labels        = self.get_fine_labels(self.data_dict)
+        self.coarse_labels      = self.get_coarse_labels(self.data_dict)
+        self.images             = self.get_images(self.data_dict)
+        self.image_folder       = self.save_images(self.images, self.file_names)
+        self.create_annotation_file()
         
-        self.test_dict          = self.get_data_dict('test')
-        self.test_file_names    = self.get_file_names(self.test_dict)
-        self.test_fine_labels   = self.get_fine_labels(self.test_dict)
-        self.test_coarse_labels = self.get_coarse_labels(self.test_dict)
-        self.test_images        = self.get_images(self.test_dict)
-        # self.save_images(self.test_images, self.test_file_names, mode = 'test')
-        self.create_annotation_file(mode = 'test')
-        
+        # Create dictionaries to map indices to classes and superclasses
         self.class_to_int, self.int_to_class, self.superclass_to_int, self.int_to_superclass = self.build_class_decoder_encoder()
         
     def unpickle(self, file):
@@ -44,11 +38,8 @@ class CIFAR(object):
             dict = pickle.load(fo, encoding = 'bytes')
         return dict 
     
-    def get_data_dict(self, mode):
-        if mode == 'train':
-            return self.unpickle(os.path.join(self.data_folder, 'train'))
-        if mode == 'test':
-            return self.unpickle(os.path.join(self.data_folder, 'test'))
+    def get_data_dict(self):
+        return self.unpickle(os.path.join(self.data_folder, self.mode))
     
     def get_file_names(self, data_dict):
         return [filename.decode('utf-8') for filename in data_dict[b'filenames']]
@@ -64,43 +55,35 @@ class CIFAR(object):
         images = images.reshape(len(images), 3, 32, 32).transpose(0, 2, 3, 1)
         return images
     
-    def save_one_image(self, one_image_numpy, filename, mode):
+    def save_one_image(self, one_image_numpy, filename):
         '''
             Save one image. Helper function for save_one_image
         '''
-        global temp 
-        temp = one_image_numpy
         PIL_image = Image.fromarray(one_image_numpy)
-        img_folder = mode + "_images"
+        img_folder = self.mode + "_images"
         img_path = self.data_folder + "/" + img_folder + "/" + filename
-        PIL_image.save(img_path, "PNG")
-        # PIL_image.save(os.path.join(self.data_folder, 'train/', filename), "PNG")
+        if not os.path.exists(img_path):
+            PIL_image.save(img_path, "PNG")
         
-    def save_images(self, images_numpy, filenames, mode):
+    def save_images(self, images_numpy, filenames):
         '''
             Save images from the input image_numpy into hard disk
         '''
-        assert mode == 'train' or mode == 'test'
-        folder_path = os.path.join(self.data_folder, mode + '_images')
+        folder_path = os.path.join(self.data_folder, self.mode + '_images') 
         if not os.path.exists(folder_path):
             os.mkdir(folder_path)
-        for img, filename in zip(images_numpy, filenames):
-            self.save_one_image(img, filename, mode)
-            break
+        for img, filename in tqdm(zip(images_numpy, filenames)):
+            self.save_one_image(img, filename)
+        return folder_path
     
-    def create_annotation_file(self, mode):
+    def create_annotation_file(self):
         '''
             Create annotation file containing image file names and labels
         '''
-        assert mode == 'train' or mode == 'test'
         annotation_dict = {}
-        file_path = self.data_folder + "/" + mode + ".csv"
-        if mode == 'train':
-            annotation_dict.update({'filename': self.train_file_names, 'fine_labels': self.train_fine_labels,
-                                    'coares_labels': self.train_coarse_labels})
-        else:
-            annotation_dict.update({'filename': self.test_file_names, 'fine_labels': self.test_fine_labels,
-                                    'coares_labels': self.test_coarse_labels})
+        annotation_dict.update({'filename': self.file_names, 'fine_label': self.fine_labels,
+                                    'coarse_label': self.coarse_labels})
+        file_path = self.data_folder + "/" + self.mode + ".csv"
         annotation_df = pd.DataFrame(annotation_dict)
         annotation_df.to_csv(file_path, index = False)
         
@@ -117,21 +100,22 @@ class CIFAR(object):
         class_to_int = {k: v for k,v in zip(fine_label_names, range(0, len(fine_label_names)))}
         int_to_class = {item[1]:item[0] for item in class_to_int.items()}
         
-        class_to_int = pd.Series(class_to_int).reset_index()
-        class_to_int.columns = ['class', 'index']
-        class_to_int_path = self.data_folder + "/" + "class_to_int.csv"
-        class_to_int.to_csv(class_to_int_path, index = False)
+        int_to_class = pd.Series(int_to_class).reset_index()
+        int_to_class.columns = ['index', 'class']
+        int_to_class_path = self.data_folder + "/" + "int_to_class.csv"
+        int_to_class.to_csv(int_to_class_path, index = False)
+        
         
         # Build super class decoder and encoder
         coarse_label_names = [each.decode('utf-8') for each in meta[b'coarse_label_names']]
         superclass_to_int = {k: v for k,v in zip(coarse_label_names, range(0, len(coarse_label_names)))}
         int_to_superclass = {item[1]:item[0] for item in superclass_to_int.items()}
         
-        superclass_to_int = pd.Series(superclass_to_int).reset_index()
-        superclass_to_int.columns = ['superclass', 'index']
-        superclass_to_int_path = self.data_folder + "/" + "superclass_to_int.csv"
-        superclass_to_int.to_csv(superclass_to_int_path, index = False)
+        int_to_superclass = pd.Series(int_to_superclass).reset_index()
+        int_to_superclass.columns = ['superclass', 'index']
+        int_to_superclass_path = self.data_folder + "/" + "int_to_superclass.csv"
+        int_to_superclass.to_csv(int_to_superclass_path, index = False)
         
         return class_to_int, int_to_class, superclass_to_int, int_to_superclass
         
-ci = CIFAR()
+ci = CIFAR(mode = 'train')
--- data_loader.py
+++ data_loader.py
@@ -4,40 +4,58 @@ Created on Sun Sep 20 01:54:34 2020
 
 @author: Admin
 """
-from config import parent_directory
+# from config import parent_directory
 import os
 os.chdir(r"C:\MountedDrive\Projects\Resnet CIFAR100")
 import torch
 import torch.utils.data as data
 from PIL import Image
-import pandas as pd
+from torchvision import transforms
 import numpy as np
-from tqdm import tqdm
-import random
-import json
+from CIFAR import CIFAR
 
 class CIFAR100Dataset(data.Dataset):
-    def __init__(self, transform, mode, batch_size, data_folder = './data'):
-        self.transform, self.mode, self.batch_size = transform, mode, batch_size
-        if self.mode == 'train':
-            annotation_path = os.path.join(data_folder, "train.csv")
+    def __init__(self, mode, batch_size = 128, transform = None, data_folder = './data'):
+        if transform is None:
+            if mode == 'train':
+                self.transform = transforms.Compose([transforms.ToTensor(),
+                                  transforms.Normalize((0.485, 0.456, 0.406),      
+                                                        (0.229, 0.224, 0.225))])
+            else:
+                self.transform = transform.ToTensor()
         else:
-            annotation_path = os.path.join(data_folder, "test.csv")
+            self.transform = transform
             
-        annotations = pd.read_csv(annotation_path)
-        class_to_int = pd.read_csv(os.path.join(data_folder, "class_to_int.csv"))
-        superclass_to_int = pd.read_csv(os.path.join(data_folder, "superclass_to_int.csv"))
+        self.mode = mode
+        self.batch_size = batch_size
+        self.data_folder = data_folder
         
+        self.cifar = CIFAR(mode = self.mode, data_folder = self.data_folder)
         
-    
     def __getitem__(self, index):
         """
           Input: index
-          Output: 3-tuple (image, class, superclass)
+          Output: 3-tuple (image, class index, superclass index)
         """
+        # get class index, superclass index, and filename
+        # class_idx = self.annotations['fine_label'].iloc[index]
+        # superclass_idx = self.annotations['coarse_label'].iloc[index]
+        # filename = self.annotations['filename'].iloc[index]
+        class_idx = self.cifar.fine_labels[index]
+        superclass_idx = self.cifar.coarse_labels[index]
+        filename = self.cifar.file_names[index]
+        
+        # Read the image and apply transformations
+        image = Image.open(os.path.join(self.cifar.image_folder, filename))
+        image = self.transform(image)
+        
+        # Return image, class index and superclass index
+        return image, class_idx, superclass_idx
+        
     def __len__(self):
-        return len(self.images)
+        return len(self.cifar.images)
 
     def get_train_indices(self):
-        return np.random.randint(low = 0, high = len(self.images), size = self.batch_size)
+        return np.random.randint(low = 0, high = len(self.cifar.images), size = self.batch_size)
     
+dataset = CIFAR100Dataset(mode = 'train')
